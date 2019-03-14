98f2e1e9128cca430369e7df327dd8f3f9c456dd
BVanEvery <ben.vanevery@gmail.com>
2019-03-14 04:49:38 -0400

    Commit prior to refactor

---
+++ ArabicToRoman/ArabicToRomanServices/ArabicToRoman.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArabicToRomanServices
+{
+    public class ArabicToRoman
+    {
+        public string ToRoman(int anArabic)
+        {
+            if (anArabic == 3)
+            {
+                return "III";
+            }
+            if (anArabic == 2)
+            {
+                return "II";
+            }
+            return "I";
+        }
+    }
+}
---
+++ ArabicToRoman/ArabicToRomanTests/ArabicToRoman_ToRomanShould.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArabicToRomanServices;
+
+namespace ArabicToRomanTests
+{
+    [TestClass]
+    public class ArabicToRoman_ToRomanShould
+    {
+        private readonly ArabicToRoman _arabicToRoman;
+
+        public ArabicToRoman_ToRomanShould()
+        {
+            this._arabicToRoman = new ArabicToRoman();
+        }
+
+        [TestMethod]
+        public void ReturnsIIIWith3()
+        {
+            var result = this._arabicToRoman.ToRoman(3);
+
+            Assert.AreEqual("III", result, false, string.Empty);
+        }
+
+        [TestMethod]
+        public void ReturnsIIWith2()
+        {
+            var result = this._arabicToRoman.ToRoman(2);
+
+            Assert.AreEqual("II", result, false, string.Empty);
+        }
+
+        [TestMethod]
+        public void ReturnsIWith1()
+        {
+            var result = this._arabicToRoman.ToRoman(1);
+
+            Assert.AreEqual("I", result, false, string.Empty);
+        }
+    }
+}
